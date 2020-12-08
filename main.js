3f7bc0ce0d1e56f4353811857446fd3f5b9bf3dd
Daniel Botea <danielmbotea@gmail.com>
2020-12-08 22:27:12 +0200

    Updates

--- main.js
+++ main.js
@@ -1,4 +1,134 @@
-import {translate} from '/data.js';
+// import {translate} from '/data.js';
+const translate = [
+  {
+    "currency": "AED",
+    "name": "United Arab Emirates Dirham"
+  },
+  {
+    "currency": "AUD",
+    "name": "Australia Dollar"
+  },
+  {
+    "currency": "BGN",
+    "name": "Bulgaria Lev"
+  },
+  {
+    "currency": "BRL",
+    "name": "Brazil Real"
+  },
+  {
+    "currency": "CAD",
+    "name": "Canada Dollar"
+  },
+  {
+    "currency": "CHF",
+    "name": "Switzerland Franc"
+  },
+  {
+    "currency": "CNY",
+    "name": "China Yuan Renminbi"
+  },
+  {
+    "currency": "CZK",
+    "name": "Czech Republic Koruna"
+  },
+  {
+    "currency": "DKK",
+    "name": "Denmark Krone"
+  },
+  {
+    "currency": "EGP",
+    "name": "Egypt Pound"
+  },
+  {
+    "currency": "EUR",
+    "name": "Euro"
+  },
+  {
+    "currency": "GBP",
+    "name": "United Kingdom Pound"
+  },
+  {
+    "currency": "HRK",
+    "name": "Croatia Kuna"
+  },
+  {
+    "currency": "HUF",
+    "name": "Hungary Forint"
+  },
+  {
+    "currency": "INR",
+    "name": "India Rupee"
+  },
+  {
+    "currency": "JPY",
+    "name": "Japan Yen"
+  },
+  {
+    "currency": "KRW",
+    "name": "Korea (South) Won"
+  },
+  {
+    "currency": "MDL",
+    "name": "Moldovian Leu"
+  },
+  {
+    "currency": "MXN",
+    "name": "Mexico Peso"
+  },
+  {
+    "currency": "NOK",
+    "name": "Norway Krone"
+  },
+  {
+    "currency": "NZD",
+    "name": "New Zealand Dollar"
+  },
+  {
+    "currency": "PLN",
+    "name": "Poland Zloty"
+  },
+  {
+    "currency": "RSD",
+    "name": "Serbia Dinar"
+  },
+  {
+    "currency": "RUB",
+    "name": "Russia Ruble"
+  },
+  {
+    "currency": "SEK",
+    "name": "Sweden Krona"
+  },
+  {
+    "currency": "THB",
+    "name": "Thailand Baht"
+  },
+  {
+    "currency": "TRy",
+    "name": "Turkey Lira"
+  },
+  {
+    "currency": "UAH",
+    "name": "Ukraine Hryvnia"
+  },
+  {
+    "currency": "USD",
+    "name": "United States Dollar"
+  },
+  {
+    "currency": "XAU",
+    "name": "One Troy Ounce of Gold"
+  },
+  {
+    "currency": "XDR",
+    "name": "Special Drawing Rights"
+  },
+  {
+    "currency": "ZAR",
+    "name": "South Africa Rand"
+  },
+]
 
 document.addEventListener('DOMContentLoaded', () => {
   let url = "https://www.bnro.ro/nbrfxrates.xml";
